4cff719ac0dae4465a9abd0e44c8e22199ca933f
Dean Moldovan <dean0x7d@gmail.com>
2015-02-24 12:03:03 +0100

    Fixed format() on VS2013

--- include/support/format.hpp
+++ include/support/format.hpp
@@ -4,8 +4,23 @@
 #include <algorithm>
 #include <cctype>
 
-#if defined(_MSC_VER) && _MSC_VER <= 1800 // a bit of a hack...
-#define snprintf _snprintf
+#if defined(_MSC_VER) && _MSC_VER <= 1800 // VS2013 does not have a C99 compliant snprintf
+# include <cstdarg>
+# define snprintf c99_snprintf
+
+inline int c99_snprintf(char* str, size_t size, const char* format, ...) {
+    int count = -1;
+    va_list ap;
+
+    va_start(ap, format);
+    if (size != 0)
+        count = _vsnprintf_s(str, size, _TRUNCATE, format, ap);
+    if (count == -1)
+        count = _vscprintf(format, ap);
+    va_end(ap);
+
+    return count;
+}
 #endif
 
 namespace fmt {
