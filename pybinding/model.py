118145d5789fea82b18decea4e90bab3001610ee
Dean Moldovan <dean0x7d@gmail.com>
2015-03-06 16:18:41 +0100

    Add band structure plot

--- pybinding/model.py
+++ pybinding/model.py
@@ -48,3 +48,29 @@ class Model(_pybinding.Model):
         sol.__class__ = _Solver
         sol.system = self.system
         return sol
+
+    def plot_bands(self, k0, k1, *ks, step, names=None):
+        # TODO: move into Solver
+        import numpy as np
+        import matplotlib.pyplot as plt
+
+        ks = [np.array(k) for k in (k0, k1) + ks]
+        energy = []
+        points = [0]
+        for start, end in zip(ks[:-1], ks[1:]):
+            num_steps = max(abs(end - start) / step)
+            k_list = (np.linspace(s, e, num_steps) for s, e in zip(start, end))
+
+            for k in zip(*k_list):
+                self.set_wave_vector(k)
+                energy.append(self.solver.energy.copy())
+            points += [len(energy)-1]
+
+        for point in points[1:-1]:
+            plt.axvline(point, color='black', ls='--')
+        plt.xticks(points, names if names else [])
+
+        plt.plot(energy, color='blue')
+        plt.xlim(0, len(energy) - 1)
+        plt.xlabel('k-space')
+        plt.ylabel('E (eV)')
