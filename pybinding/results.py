eebd9624fe1ba217421fd9c59379750c765d3f23
Dean Moldovan <dean0x7d@gmail.com>
2015-07-02 17:28:17 +0200

    Make SpatialMap pickleable

--- pybinding/results.py
+++ pybinding/results.py
@@ -1,10 +1,12 @@
+from copy import copy
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.sparse import csr_matrix
 
 from . import pltutils
 from .utils import with_defaults
-from .system import plot_sites, plot_hoppings
+from .system import Positions, plot_sites, plot_hoppings
 from .support.pickle import pickleable
 
 __all__ = ['LDOSpoint', 'SpatialMap']
@@ -24,10 +26,12 @@ class LDOSpoint:
         pltutils.despine()
 
 
+@pickleable
 class SpatialMap:
-    def __init__(self, data, xyz, sublattice, hoppings: csr_matrix):
-        self.data = data
-        self.xyz = xyz
+    def __init__(self, data: np.ndarray, pos: Positions,
+                 sublattice: np.ndarray, hoppings: csr_matrix):
+        self.data = data  # 1d array of data which corresponds to (x, y, z) coordinates
+        self.pos = Positions(*pos)  # make sure it is Positions
         self.sublattice = sublattice
         self.hoppings = hoppings
 
@@ -35,42 +39,24 @@ class SpatialMap:
     def from_system(cls, data, system):
         return cls(data, system.positions, system.sublattice, system.matrix.tocsr())
 
-    @classmethod
-    def from_file(cls, file_name):
-        file = np.load(file_name)
-        xyz = tuple(file[v] for v in 'xyz')
-        m = tuple(file[v] for v in ['m_data', 'm_indices', 'm_indptr'])
-        return cls(file['data'], xyz, file['sublattice'], csr_matrix(m, file['m_shape']))
-
     def copy(self) -> 'SpatialMap':
-        import copy
-        return copy.copy(self)
-
-    def save(self, file_name):
-        x, y, z = self.xyz
-        m = self.hoppings
-
-        np.savez_compressed(
-            file_name,
-            data=self.data, x=x, y=y, z=z, sublattice=self.sublattice,
-            m_data=m.data, m_indices=m.indices, m_indptr=m.indptr, m_shape=m.shape
-        )
+        return copy(self)
 
     def save_txt(self, file_name):
-        with open(file_name+'.dat', 'w') as file:
+        with open(file_name + '.dat', 'w') as file:
             file.write('# {:12}{:13}{:13}\n'.format('x(nm)', 'y(nm)', 'data'))
-            for x, y, value in zip(self.xyz[0], self.xyz[1], self.data):
-                file.write(("{:13.5e}"*3 + '\n').format(x, y, value))
+            for x, y, d in zip(self.pos.x, self.pos.y, self.data):
+                file.write(("{:13.5e}" * 3 + '\n').format(x, y, d))
 
     def filter(self, idx):
         self.data = self.data[idx]
         self.sublattice = self.sublattice[idx]
-        self.xyz = map(lambda v: v[idx], self.xyz)
+        self.pos = Positions(*map(lambda v: v[idx], self.pos))
         self.hoppings = self.hoppings[idx][:, idx]
 
     def crop(self, x=None, y=None):
         xlim, ylim = x, y
-        x, y, _ = self.xyz
+        x, y, _ = self.pos
         idx = np.ones(x.size, dtype=bool)
         if xlim:
             idx = np.logical_and(idx, x >= xlim[0], x <= xlim[1])
@@ -78,8 +64,11 @@ class SpatialMap:
             idx = np.logical_and(idx, y >= ylim[0], y <= ylim[1])
         self.filter(idx)
 
+    def clip(self, v_min, v_max):
+        self.data = np.clip(self.data, v_min, v_max)
+
     def convolve(self, sigma=0.25):
-        x, y, _ = self.xyz
+        x, y, _ = self.pos
         r = np.sqrt(x**2 + y**2)
 
         data = np.empty_like(self.data)
@@ -99,23 +88,24 @@ class SpatialMap:
         ax.set_xticks(ax.get_xticks()[1:-1])
         ax.set_yticks(ax.get_yticks()[1:-1])
 
-    def plot_contourf(self, num_levels=50, cbar_props=None, **kwargs):
-        levels = np.linspace(self.data.min(), self.data.max(), num_levels)
-        x, y, _ = self.xyz
-        contour = plt.tricontourf(x, y, self.data,
-                                  **with_defaults(kwargs, cmap='YlGnBu', levels=levels))
-
-        if cbar_props is not False:
-            cbar_props = cbar_props if cbar_props else {}
-            pltutils.colorbar(**with_defaults(cbar_props, format='%.2f'))
-
+    def plot_pcolor(self, **kwargs):
+        x, y, _ = self.pos
+        pcolor = plt.tripcolor(x, y, self.data,
+                               **with_defaults(kwargs, cmap='YlGnBu', shading='gouraud'))
         self._decorate_plot()
-        return contour
+        return pcolor
+
+    def plot_contourf(self, num_levels=50, **kwargs):
+        levels = np.linspace(self.data.min(), self.data.max(), num=num_levels)
+        x, y, _ = self.pos
+        contourf = plt.tricontourf(x, y, self.data,
+                                   **with_defaults(kwargs, cmap='YlGnBu', levels=levels))
+        self._decorate_plot()
+        return contourf
 
     def plot_contour(self, **kwargs):
-        x, y, _ = self.xyz
+        x, y, _ = self.pos
         contour = plt.tricontour(x, y, self.data, **kwargs)
-
         self._decorate_plot()
         return contour
 
@@ -126,12 +116,11 @@ class SpatialMap:
         ax.set_xlabel('x (nm)')
         ax.set_ylabel('y (nm)')
 
-        x, y, z = self.xyz
         radius = site_radius[0] + site_radius[1] * self.data / self.data.max()
-        collection = plot_sites(ax, (x, y, z), self.data, radius,
+        collection = plot_sites(ax, self.pos, self.data, radius,
                                 **with_defaults(site_props, cmap='YlGnBu'))
 
-        plot_hoppings(ax, (x, y, z), self.hoppings.tocoo(), hopping_width,
+        plot_hoppings(ax, self.pos, self.hoppings.tocoo(), hopping_width,
                       **with_defaults(hopping_props, colors='#bbbbbb'))
 
         pltutils.colorbar(collection, **with_defaults(cbar_props))
