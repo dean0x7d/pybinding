a15a4e21762ca3fe49e9e460b5077ac388ae342f
Dean Moldovan <dean0x7d@gmail.com>
2015-11-16 15:22:03 +0100

    Add docstrings to Lattice

--- pybinding/lattice.py
+++ pybinding/lattice.py
@@ -14,46 +14,132 @@ __all__ = ['Lattice', 'make_lattice', 'square']
 
 @pickleable(props='sublattices hopping_energies min_neighbors')
 class Lattice(_cpp.Lattice):
+    """Holds the specification of a tight-binding lattice
+
+    Parameters
+    ----------
+    a1, a2, a3 : array_like
+        Primitive vectors of a Bravais lattice. A valid lattice must have at least
+        one primitive vector (`a1`), thus forming a simple 1-dimensional lattice.
+        If `a2` is also specified, a 2D lattice is created. Passing values for all
+        three vectors will create a 3D lattice.
+
+    Attributes
+    ----------
+    sublattice_ids : OrderedDict
+        Map from user-friendly sublattice names to unique IDs. This may also be
+        accessed as `lattice[sublattice_name]`.
+    hopping_ids : dict
+        Map from user-friendly hopping names to unique IDs.
+    """
+
     def __init__(self, a1, a2=None, a3=None):
         vectors = (np.atleast_1d(v) for v in (a1, a2, a3) if v is not None)
         super().__init__(*vectors)
-        self.sublattice_ids = OrderedDict()  # indexed by sublattice name
-        self.hopping_ids = {}  # indexed by hopping name
+        self.sublattice_ids = OrderedDict()
+        self.hopping_ids = {}
 
-    def __getitem__(self, key):
-        """Get sublattice ID from name."""
-        if isinstance(key, str):
-            if key not in self.sublattice_ids:
-                raise KeyError("There is no sublattice named '{}'".format(key))
-            return self.sublattice_ids[key]
+    def __getitem__(self, name):
+        """Get sublattice ID from its user-friendly `name`"""
+        if isinstance(name, str):
+            if name not in self.sublattice_ids:
+                raise KeyError("There is no sublattice named '{}'".format(name))
+            return self.sublattice_ids[name]
         else:  # an ID was given instead of a name, verify it
-            if key not in self.sublattice_ids.values():
-                raise KeyError("There is no sublattice with ID = {}".format(key))
-            return key
+            sub_id = name
+            if sub_id not in self.sublattice_ids.values():
+                raise KeyError("There is no sublattice with ID = {}".format(sub_id))
+            return sub_id
 
     @property
     def ndim(self):
+        """The dimensionality of the lattice: number of primitive vectors"""
         return len(self.vectors)
 
-    def register_hopping_energies(self, mapping: dict):
+    def register_hopping_energies(self, mapping):
+        """Register a mapping of user-friendly names to hopping energies
+
+        Parameters
+        ----------
+        mapping : dict
+            Keys are user-friendly hopping names and values are the numeric values
+            of the hopping energy.
+        """
         for name, energy in sorted(mapping.items(), key=lambda item: item[0]):
             if name in self.hopping_ids:
                 raise KeyError("Hopping '{}' already exists".format(name))
             self.hopping_ids[name] = super()._register_hopping_energy(energy)
 
-    def add_one_sublattice(self, name, offset, onsite_potential=0.0, alias=None):
+    def add_one_sublattice(self, name, offset, onsite_energy=0.0, alias=None):
+        """Add a new sublattice
+
+        Parameters
+        ----------
+        name : str
+            User-friendly identifier. The unique sublattice ID can later be accessed
+            via this sublattice name as `lattice[sublattice_name]`.
+        offset : array_like
+            Cartesian position with respect to the origin.
+        onsite_energy : float, optional
+            Onsite energy to be applied only to sites of this sublattice.
+        alias : str, optional
+            Given the name of a previously defined sublattice, the new sublattice
+            is created as an alias for the old one. This is useful when defining
+            a supercell which contains multiple sites of one sublattice family at
+            different positions.
+        """
         if name in self.sublattice_ids:
             raise KeyError("Sublattice '{}' already exists".format(name))
 
         offset = np.atleast_1d(offset)
         alias = self.__getitem__(alias) if alias is not None else -1
-        self.sublattice_ids[name] = super()._add_sublattice(offset, onsite_potential, alias)
+        self.sublattice_ids[name] = super()._add_sublattice(offset, onsite_energy, alias)
 
     def add_sublattices(self, *sublattices):
+        """Add multiple new sublattices
+
+        Parameters
+        ----------
+        *sublattices
+            Each element should be a tuple containing the arguments for
+            a `add_one_sublattice()` method call. See example.
+
+        Example
+        -------
+        These three calls:
+            lattice.add_one_sublattice('a', [0, 0], 0.5)
+            lattice.add_one_sublattice('b', [0, 1], 0.0)
+            lattice.add_one_sublattice('c', [1, 0], 0.3)
+
+        Can be replaced with a single call to:
+            lattice.add_sublattices(
+                ('a', [0, 0], 0.5),
+                ('b', [0, 1], 0.0),
+                ('c', [1, 0], 0.3)
+            )
+        """
         for sub in sublattices:
             self.add_one_sublattice(*sub)
 
     def add_one_hopping(self, relative_index, from_sublattice, to_sublattice, energy):
+        """Add a new hopping
+
+        For each new hopping, its Hermitian conjugate is added automatically. Doing so
+        manually, i.e. adding a hopping which is the Hermitian conjugate of an existing
+        one, will result in an exception being raised.
+
+        Parameters
+        ----------
+        relative_index : array_like of int
+            Difference of the indices of the source and destination unit cells.
+        from_sublattice : str
+            Name of the sublattice in the source unit cell.
+        to_sublattice : str
+            Name of the sublattice in the destination unit cell.
+        energy : float or str
+            The numeric value of the hopping energy or the name of a previously
+            registered hopping.
+        """
         relative_index = np.atleast_1d(relative_index)
         from_sub, to_sub = map(self.__getitem__, (from_sublattice, to_sublattice))
 
@@ -66,10 +152,39 @@ class Lattice(_cpp.Lattice):
             raise KeyError("There is no hopping named '{}'".format(energy))
 
     def add_hoppings(self, *hoppings):
+        """Add multiple new hoppings
+
+        Parameters
+        ----------
+        *hoppings
+            Each element should be a tuple containing the arguments for
+            a `add_one_hopping()` method call. See example.
+
+        Example
+        -------
+        These three calls:
+            lattice.add_one_hopping([0, 0], 'a', 'b', 0.8)
+            lattice.add_one_hopping([0, 1], 'a', 'a', 0.3)
+            lattice.add_one_hopping([1, 1], 'a', 'b', 0.8)
+
+        Can be replaced with a single call to:
+            lattice.add_hoppings(
+                ([0, 0], 'a', 'b', 0.8),
+                ([0, 1], 'a', 'a', 0.3),
+                ([1, 1], 'a', 'b', 0.8),
+            )
+        """
         for hop in hoppings:
             self.add_one_hopping(*hop)
 
     def add_hopping_matrices(self, *pairs):
+        """Add hoppings in the matrix format
+
+        Parameters
+        ----------
+        *pairs
+            Each element is a tuple of `relative_index` and `hopping_matrix`.
+        """
         for relative_index, matrix in pairs:
             for (from_sub, to_sub), hopping_energy in np.ndenumerate(matrix):
                 if hopping_energy == 0:
@@ -82,7 +197,7 @@ class Lattice(_cpp.Lattice):
                 self.add_one_hopping(relative_index, from_sub, to_sub, hopping_energy)
 
     def reciprocal_vectors(self) -> list:
-        """Calculate reciprocal space lattice vectors
+        """Calculate the reciprocal space lattice vectors
 
         >>> lat = Lattice(a1=[0, 1], a2=[0.5, 0.5])
         >>> np.allclose(lat.reciprocal_vectors(), [[4*pi, 0, 0], [-2*pi, 2*pi, 0]])
@@ -101,7 +216,7 @@ class Lattice(_cpp.Lattice):
         >>> np.allclose(lat.brillouin_zone(), [[-2*pi, 0], [0, -2*pi], [2*pi, 0], [0, 2*pi]])
         True
         """
-        def perp(vec):
+        def _perp(vec):
             """Make 'a*x + b*y = c' line which is perpendicular to the vector"""
             v_rot90 = [-vec[1], vec[0], 0]
             x1, y1, _ = 0.5 * (vec + v_rot90)
@@ -111,7 +226,7 @@ class Lattice(_cpp.Lattice):
             c = x2 * y1 - x1 * y2
             return a, b, c
 
-        def intersection(line1, line2):
+        def _intersection(line1, line2):
             """Return (x, y) coordinates of the intersection via Cramer's rule"""
             d = np.linalg.det([line1[:2], line2[:2]])
             dx = np.linalg.det([line1[1:], line2[1:]])
@@ -128,7 +243,7 @@ class Lattice(_cpp.Lattice):
 
             vertices = []
             for v1 in vectors:
-                intersections = [intersection(perp(v1), perp(v2)) for v2 in vectors]
+                intersections = [_intersection(_perp(v1), _perp(v2)) for v2 in vectors]
                 intersections = filter(lambda v: v is not None, intersections)
 
                 # keep only the two closest to (0, 0)
@@ -159,11 +274,26 @@ class Lattice(_cpp.Lattice):
                                   fontsize='large', bbox=dict(lw=0, alpha=0.6))
 
     def plot_vectors(self, position):
-        """Plot lattice vectors in xy plane"""
+        """Plot lattice vectors in the xy plane
+
+        Parameters
+        ----------
+        position : array_like
+            Cartesian position to be used as the origin for the vectors.
+        """
         self._plot_vectors(self.vectors, position)
 
     def plot(self, vector_position='center', **kwargs):
-        """Illustrate the lattice by plotting the primitive cell and its nearest neighbors"""
+        """Illustrate the lattice by plotting the primitive cell and its nearest neighbors
+
+        Parameters
+        ----------
+        vector_position : array_like or 'center'
+            Cartesian position to be used as the origin for the lattice vectors.
+            By default the origin is placed in the center of the primitive cell.
+        **kwargs
+            Forwarded to `System.plot()`.
+        """
         import pybinding as pb
         # reuse model plotting code (kind of meta)
         model = pb.Model(self, pb.symmetry.translational())
@@ -204,7 +334,13 @@ class Lattice(_cpp.Lattice):
         pltutils.set_min_range(abs(max(y) - min(y)), 'y')
 
     def plot_brillouin_zone(self, **kwargs):
-        """Plot the Brillouin zone and reciprocal lattice vectors"""
+        """Plot the Brillouin zone and reciprocal lattice vectors
+
+        Parameters
+        ----------
+        **kwargs
+            Forwarded to `plt.plot()`.
+        """
         ax = plt.gca()
         ax.set_aspect('equal')
         ax.set_xlabel(r"$k_x (nm^{-1})$")
