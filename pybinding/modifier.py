605ed5f4fa038223b2f33cf47d0cdcc82a221583
Dean Moldovan <dean0x7d@gmail.com>
2015-11-17 13:47:44 +0100

    Add docstrings to the modifier module

--- pybinding/modifier.py
+++ pybinding/modifier.py
@@ -1,3 +1,8 @@
+"""Modifier decorators
+
+Used to create functions which express some feature of a tight-binding model,
+such as various fields, defects or geometric deformations.
+"""
 import inspect
 import numpy as np
 
@@ -9,7 +14,15 @@ __all__ = ['site_state_modifier', 'site_position_modifier',
 
 
 def _check_modifier_spec(func, keywords):
-    """Make sure the arguments are specified correctly"""
+    """Make sure the arguments are specified correctly
+
+    Parameters
+    ----------
+    func : callable
+        The function which is to become a modifier.
+    keywords : list
+        Used to check that `func` arguments are correct.
+    """
     argnames = inspect.signature(func).parameters.keys()
     unexpected = ", ".join([name for name in argnames if name not in keywords])
     if unexpected:
@@ -18,13 +31,25 @@ def _check_modifier_spec(func, keywords):
                            "Arguments must be any of: {expected}".format(**locals()))
 
 
-def _check_modifier_return(modifier, keywords: list, num_return: int, maybe_complex: bool):
-    """Make sure the modifier return the correct type and size"""
+def _check_modifier_return(modifier, num_arguments, num_return, maybe_complex):
+    """Make sure the modifier returns the correct type and size
+
+    Parameters
+    ----------
+    modifier
+        The modifier being checked.
+    num_arguments : int
+        Expected number of modifier arguments.
+    num_return : int
+        Expected number of return values.
+    maybe_complex : bool
+        The modifier may return a complex result even if the input is real.
+    """
     in_shape = 10,
     in_data = np.random.rand(*in_shape).astype(np.float16)
 
     try:
-        out_data = modifier.apply(*(in_data,) * len(keywords))
+        out_data = modifier.apply(*(in_data,) * num_arguments)
     except AttributeError as e:
         if "astype" in str(e):  # known issue
             raise RuntimeError("Modifier must return numpy.ndarray")
@@ -42,11 +67,30 @@ def _check_modifier_return(modifier, keywords: list, num_return: int, maybe_comp
         raise RuntimeError("This modifier must not return complex values")
 
 
-def _make_modifier(func, base_modifier, keywords: str, num_return=1, maybe_complex=False):
-    keywords = [word.strip(",") for word in keywords.split()]
+def _make_modifier(func, kind, keywords, num_return=1, maybe_complex=False):
+    """Turn a regular function into a modifier of the desired kind
+
+    Parameters
+    ----------
+    func : callable
+        The function which is to become a modifier.
+    kind : object
+        Modifier base class.
+    keywords : str
+        String of comma separated names: the expected arguments of a modifier function.
+    num_return : int
+        Expected number of return values.
+    maybe_complex : bool
+        The modifier may return a complex result even if the input is real.
+
+    Returns
+    -------
+    Modifier
+    """
+    keywords = [word.strip() for word in keywords.split(",")]
     _check_modifier_spec(func, keywords)
 
-    class Modifier(base_modifier):
+    class Modifier(kind):
         argnames = tuple(inspect.signature(func).parameters.keys())
         try:
             callsig = get_call_signature(up=3)
@@ -85,22 +129,105 @@ def _make_modifier(func, base_modifier, keywords: str, num_return=1, maybe_compl
             return np.iscomplexobj(ret)
 
     modifier = Modifier()
-    _check_modifier_return(modifier, keywords, num_return, maybe_complex)
+    _check_modifier_return(modifier, len(keywords), num_return, maybe_complex)
     return modifier
 
 
 def site_state_modifier(func):
+    """Modify the state (valid or invalid) of lattice sites, e.g. to create vacancies
+
+    Parameters
+    ----------
+    func : callable
+        The function parameters must be a combination of any number of the following:
+        state : ndarray of bool
+            Indicates if a lattice site is valid. Invalid sites will be removed from
+            the model after all modifiers have been applied.
+        x, y, z : ndarray
+            Lattice site position.
+        sub : ndarray of int
+            Sublattice ID. Can be checked for equality with `Lattice[sublattice_name]`.
+
+        Modifier returns:
+        ndarray
+            A modified `state` argument or an `ndarray` of the same dtype and shape.
+
+    Returns
+    -------
+    Modifier
+    """
     return _make_modifier(func, _cpp.SiteStateModifier, keywords="state, x, y, z, sub")
 
 
 def site_position_modifier(func):
+    """Modify the position of lattice sites, e.g. to apply geometric deformations
+
+    Parameters
+    ----------
+    func : callable
+        The function parameters must be a combination of any number of the following:
+        x, y, z : ndarray
+            Lattice site position.
+        sub : ndarray of int
+            Sublattice ID. Can be checked for equality with `Lattice[sublattice_name]`.
+
+        Modifier returns:
+        tuple of ndarray
+            Modified 'x, y, z' arguments or 3 `ndarray`s of the same dtype and shape.
+
+    Returns
+    -------
+    Modifier
+    """
     return _make_modifier(func, _cpp.PositionModifier, keywords="x, y, z, sub", num_return=3)
 
 
 def onsite_energy_modifier(func):
+    """Modify the onsite energy, e.g. to apply an electric field
+
+    Parameters
+    ----------
+    func : callable
+        The function parameters must be a combination of any number of the following:
+        potential : ndarray
+            The onsite energy.
+        x, y, z : ndarray
+            Lattice site position.
+        sub : ndarray of int
+            Sublattice ID. Can be checked for equality with `Lattice[sublattice_name]`.
+
+        Modifier returns:
+        ndarray
+            A modified `potential` argument or an `ndarray` of the same dtype and shape.
+
+    Returns
+    -------
+    Modifier
+    """
     return _make_modifier(func, _cpp.OnsiteModifier, keywords="potential, x, y, z, sub")
 
 
 def hopping_energy_modifier(func):
+    """Modify the hopping energy, e.g. to apply a magnetic field
+
+    Parameters
+    ----------
+    func : callable
+        The function parameters must be a combination of any number of the following:
+        hopping : ndarray
+            The hopping energy between two sites.
+        x1, y1, z1, x2, y2, z2 : ndarray
+            Positions of the two lattice sites connected by the hopping parameter.
+        hop_id : ndarray of int
+            Hopping ID. Check for equality with `Lattice.hopping_ids[hopping_name]`.
+
+        Modifier returns:
+        ndarray
+            A modified `hopping` argument or an `ndarray` of the same dtype and shape.
+
+    Returns
+    -------
+    Modifier
+    """
     return _make_modifier(func, _cpp.HoppingModifier, maybe_complex=True,
                           keywords="hopping, hop_id, x1, y1, z1, x2, y2, z2")
