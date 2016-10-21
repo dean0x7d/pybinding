3ece6a5afab8c1a8eadf3ed90d9a1eae349ea071
Dean Moldovan <dean0x7d@gmail.com>
2016-10-21 15:18:40 +0200

    Tweak CMake configuration

--- cppcore/cmake/cppformat.cmake
+++ cppcore/cmake/cppformat.cmake
@@ -5,6 +5,6 @@ download_dependency(cppformat 2.0.0
 add_library(cppformat STATIC EXCLUDE_FROM_ALL ${CPPFORMAT_INCLUDE_DIR}/format.cc)
 target_include_directories(cppformat SYSTEM PUBLIC ${CPPFORMAT_INCLUDE_DIR})
 if(NOT WIN32)
-    target_compile_options(cppformat PUBLIC -std=c++11)
+    target_compile_options(cppformat PUBLIC ${PB_CPP_STANDARD})
 endif()
 set_target_properties(cppformat PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
