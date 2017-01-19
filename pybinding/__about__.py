3ae42b9dbd5d814e243307c62ad0f9880f199c2a
Dean Moldovan <dean0x7d@gmail.com>
2017-01-19 11:53:24 +0100

    Update year in license

--- pybinding/__about__.py
+++ pybinding/__about__.py
@@ -5,6 +5,6 @@ __summary__ = "Package for tight-binding calculations"
 __url__ = "https://github.com/dean0x7d/pybinding"
 
 __author__ = "Dean Moldovan"
-__copyright__ = "2015-2016, " + __author__
+__copyright__ = "2015-2017, " + __author__
 __email__ = "dean0x7d@gmail.com"
 __license__ = "BSD"
